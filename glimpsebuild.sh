10906a75f494f7e02cc9d0d756e0ca8cbfc0db83
Aqua <ollie@ollierobinson.co.uk>
2025-11-16 22:48:43 +0000

    zip build folders

--- glimpsebuild.sh
+++ glimpsebuild.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: glimpsebuild <out-dir> <runtime> <version>"
+  echo "Usage: glimpsebuild <out_name> <runtime> <version>"
   exit 2
 fi
 
-OUT_DIR="$(pwd)/$1"
+OUT_NAME=$1
+BUILD_DIR_NAME="${OUT_NAME}"
+BUILD_DIR="$(pwd)/$BUILD_DIR_NAME"
 RUNTIME=$2
 VERSION=$3
-PLUGINS_DIR="$OUT_DIR/Plugins"
-PLUGINS_DIR_TEMP="$OUT_DIR/PluginsTEMP"
+PLUGINS_DIR="$BUILD_DIR/Plugins"
+PLUGINS_DIR_TEMP="$BUILD_DIR/PluginsTEMP"
 
 # Ensure output directory is reset.
-rm -rf $OUT_DIR || exit 1
-mkdir -p $OUT_DIR || exit 1
+rm -rf $BUILD_DIR || exit 1
+mkdir -p $BUILD_DIR || exit 1
 
 # Publish main glimpse program.
-dotnet publish -c Release -r $RUNTIME -o $OUT_DIR -p:Version="$VERSION" src/Glimpse || exit 1
+dotnet publish -c Release -r $RUNTIME -o $BUILD_DIR -p:Version="$VERSION" src/Glimpse || exit 1
 
 # As glimpse uses MixrSharp as a project reference rather than a nuget, we must delete the library file that doesn't
 # match the output runtime.
-pushd $OUT_DIR
+pushd $BUILD_DIR
 if [[ $RUNTIME == "win"* ]]; then
   rm libmixr.so || exit 1
 elif [[ $RUNTIME == "linux"* ]]; then
@@ -35,10 +37,10 @@ for dir in Plugins/*; do
   if [ -f "Dependencies.txt" ]; then
     dir_name="${dir##*/}"
     plugin_temp_dir="$PLUGINS_DIR_TEMP/$dir_name"
-    plugin_out_dir="$PLUGINS_DIR/$dir_name"
+    plugin_BUILD_DIR="$PLUGINS_DIR/$dir_name"
     
     mkdir -p $plugin_temp_dir
-    mkdir -p $plugin_out_dir
+    mkdir -p $plugin_BUILD_DIR
     
     # Publish to a temporary directory first.
     dotnet publish -c Release -r $RUNTIME -o $plugin_temp_dir -p:Version="$VERSION" . || exit 1
@@ -55,10 +57,18 @@ for dir in Plugins/*; do
         fi
       fi
       # Copy the dependency to the main plugins directory.
-      cp "$plugin_temp_dir/$dep" "$plugin_out_dir" || exit 1
+      cp "$plugin_temp_dir/$dep" "$plugin_BUILD_DIR" || exit 1
     done <"Dependencies.txt"
   fi
   popd || exit 1
 done
 
 rm -rf $PLUGINS_DIR_TEMP || exit 1
+
+if [[ $RUNTIME == "win"* ]]; then
+  zip -r "$OUT_NAME.zip" "$BUILD_DIR_NAME/" || exit 1
+elif [[ $RUNTIME == "linux"* ]]; then
+  tar -czvf "$OUT_NAME.tar.gz" "$BUILD_DIR_NAME/" || exit 1
+fi
+
+rm -rf $BUILD_DIR
